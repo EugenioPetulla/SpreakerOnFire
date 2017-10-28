6196e9d0a576a256a55fa960df59a11535082205
Eugenio Petullà <eugeniopetulla@yahoo.it>
2017-10-28 20:39:56 +0200

    Add Sass color vars and gulp-sass task

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ var gulp  = require('gulp'),
 browserSync = require('browser-sync').create();
 var cleanCSS = require('gulp-clean-css');
 var rename = require('gulp-rename');
+var sass = require('gulp-sass');
 
 // Static server
 gulp.task('browser-sync', function() {
@@ -20,16 +21,23 @@ gulp.task('live-reload', function(done) {
     done();
 });
 
+gulp.task('sass', function () {
+  return gulp.src('./sass/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+});
+
 // Minify and rename css
 gulp.task('minify-css', function() {
-  return gulp.src(['css/*.css', '!css/*.min.css'])
+  return gulp.src(['./css/*.css', '!./css/*.min.css'])
     .pipe(cleanCSS({compatibility: 'ie8'}))
     .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest('css'));
+    .pipe(gulp.dest('./css'));
 });
 
 // create a default task and just log a message
-gulp.task('default', ['minify-css', 'browser-sync'], function() {
-	gulp.watch(['css/*.css'], ['minify-css']);
-	gulp.watch(['css/*.css', '*.html', 'js/*.js'], ['live-reload']);
+gulp.task('default', ['sass', 'minify-css', 'browser-sync'], function() {
+	gulp.watch(['./sass/*.scss'], ['sass', 'minify-css']);
+    gulp.watch(['./css/*.css'], ['minify-css']);
+	gulp.watch(['./sass/*.scss', './css/*.css', '*.html', './js/*.js'], ['live-reload']);
 });
